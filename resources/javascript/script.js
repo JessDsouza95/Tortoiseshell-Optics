163f7b6f6a20012bb4b5ed2fc34590d22f32b5e8
Jess D'Souza <jess.dsouza@hotmail.com>
2018-02-11 22:15:26 +0400

    Test Upload

---
+++ resources/javascript/script.js
@@ -0,0 +1,19 @@
+alert("test");
+
+
+var html = '';
+var red;
+var green;
+var blue;
+var rgbColor;
+
+
+
+for (var i = 0; i < 10 ; i++) {
+	red = Math.floor(Math.random() * 256 );
+	green = Math.floor(Math.random() * 256 );
+	blue = Math.floor(Math.random() * 256 );
+	rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+	html += '<div style="background-color:' + rgbColor + '"></div>';
+	document.write(html);
+}
